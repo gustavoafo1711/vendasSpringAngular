0a7cc87d342a8567e93b9f25d581f9ccffcb1775
gustavo <gustavo@gustavo-Aspire-5741Z>
2022-11-17 10:32:47 -0300

    Hello World

---
+++ vendasSpringAngular/src/test/java/com/gustavo/vendasSpringAngular/VendasSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.vendasSpringAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
