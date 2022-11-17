05b97f85aa90c812e11ed730f78623ebe0856e6b
gustavo <gustavo@gustavo-Aspire-5741Z>
2022-11-17 10:47:02 -0300

    Configuration & Bean

---
+++ vendasSpringAngular/src/main/java/com/gustavo/vendasSpringAngular/MinhaConfiguration.java
@@ -0,0 +1,15 @@
+package com.gustavo.vendasSpringAngular;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+	
+	@Bean(name = "applicationName")
+	public String applicationName() {
+		return "Sistema de Vendas";
+		
+	}
+
+}
