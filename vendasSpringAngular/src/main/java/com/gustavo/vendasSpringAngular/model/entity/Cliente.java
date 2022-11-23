5f01c77cc1d63004e0e08a976a7c293c9d3e2071
gustavo <gustavoafo@hotmail.com>
2022-11-23 16:09:48 -0300

    Final Desenvolvimento API Clientes

--- vendasSpringAngular/src/main/java/com/gustavo/vendasSpringAngular/model/entity/Cliente.java
+++ vendasSpringAngular/src/main/java/com/gustavo/vendasSpringAngular/model/entity/Cliente.java
@@ -2,15 +2,15 @@ package com.gustavo.vendasSpringAngular.model.entity;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -22,13 +22,21 @@ public class Cliente {
 	private Integer id;
 	
 	@Column(nullable = false, length = 150)
+	@NotEmpty(message = "{campo.nome.obrigatorio}")
 	private String nome;
 	
 	@Column(nullable = false, length = 11)
+	@CPF(message = "{campo.cpf.invalido}")
+	@NotNull(message = "{campo.cpf.obrigatorio}")
 	private String cpf;
 	
-	@Column(name = "data_cadastro")
+	@Column(name = "data_cadastro", updatable = false)
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataCadastro;
-	
+
+	@PrePersist
+	public void prePersist(){
+		setDataCadastro(LocalDate.now());
+	}
 
 }
