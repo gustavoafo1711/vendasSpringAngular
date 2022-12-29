65e26c0abe158a90d6338605e7c77e019e6d8f0d
gustavo <gustavoafo@hotmail.com>
2022-12-29 18:06:53 -0300

    Método de Pesquisa de Serviços Prestados

--- vendasSpringAngular/src/main/java/com/gustavo/vendasSpringAngular/model/repository/ClienteRepository.java
+++ vendasSpringAngular/src/main/java/com/gustavo/vendasSpringAngular/model/repository/ClienteRepository.java
@@ -1,9 +1,20 @@
 package com.gustavo.vendasSpringAngular.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gustavo.vendasSpringAngular.model.entity.Cliente;
+import com.gustavo.vendasSpringAngular.model.entity.ServicoPrestado;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Query(" select s from ServicoPrestado s join s.cliente c "
+			+ "where upper( c.nome ) like upper( :nome ) and MONTH( s.data ) =:mes ")
+	List<ServicoPrestado> findByNomeClienteAndMes(
+			@Param("nome") String nome, @Param("mes") Integer mes);
+	
+	
 }
--- vendasSpringAngular/src/main/java/com/gustavo/vendasSpringAngular/rest/ServicoPrestadoController.java
+++ vendasSpringAngular/src/main/java/com/gustavo/vendasSpringAngular/rest/ServicoPrestadoController.java
@@ -2,11 +2,14 @@ package com.gustavo.vendasSpringAngular.rest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -50,5 +53,17 @@ public class ServicoPrestadoController {
 		
 		return servicoPrestadoRepository.save(servicoPrestado);
 	}
+	
+	@GetMapping
+	public  List<ServicoPrestado> pesquisar(
+			@RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+			@RequestParam(value = "mes", required = false) Integer mes
+			){
+		
+		return clienteRepository.findByNomeClienteAndMes("%" + nome + "%", mes);
+	}
+	
+	
+	
 
 }
